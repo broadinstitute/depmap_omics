d2f2294393198a43200f55377a28cd4158e9df75
Jérémie Kalfon <jkobject@gmail.com>
2019-09-18 16:15:58 -0400

    improving the pipelines and creating CCLF_processing

---
+++ src/CCLF_processing.py
@@ -0,0 +1,6 @@
+def getReport():
+    """
+    """
+    # if we get many condition: merge and display the one that has been selected)
+    # if we get WES data: add on the side as additional data and merge CNplot
+    # if we get primary: add on the side as additional data and merge CNplot
