3a4e53d69f9f65b9b967afb254dab5edaf23116b
Jérémie Kalfon <jkobject@gmail.com>
2019-09-24 13:52:29 -0400

    adding new processing for CCLF, comparing filter and unfilterd mafs computing PONs

--- src/CCLF_processing.py
+++
@@ -1,6 +0,0 @@
-def getReport():
-    """
-    """
-    # if we get many condition: merge and display the one that has been selected)
-    # if we get WES data: add on the side as additional data and merge CNplot
-    # if we get primary: add on the side as additional data and merge CNplot
