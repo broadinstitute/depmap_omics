66f3f73a69cf355b0b5ee73f9904a8de24493aba
Javad Noorbakhsh <jnoorbak@broadinstitute.org>
2021-01-13 17:56:06 +0000

    put into loop

--- qc/get_cell_line_set.py
+++ qc/get_cell_line_set.py
@@ -13,20 +13,14 @@ def tcget(name=None, version=None, file=None):
     assert all([x.startswith('ACH-') for x in all_21Q1_gene_cn])    
     return arxspan_ids
 
-def get_all_arxspans(name='internal-20q3-00d0', version=9):
+def get_all_arxspans(name='internal-20q3-00d0', version=9, 
+        files=['CCLE_expression', 'CCLE_expression_full', 'CCLE_fusions',
+               'CCLE_fusions_unfiltered', 'CCLE_gene_cn', 'CCLE_mutations',
+               'CCLE_RNAseq_reads', 'CCLE_RNAseq_transcripts', 'CCLE_segment_cn']):
     # get the set of all the arxspans from a specific Taiga upload
-    CCLE_expression = tcget(name=name, version=version, file='CCLE_expression')
-    CCLE_expression_full = tcget(name=name, version=version, file='CCLE_expression_full')
-    CCLE_fusions = tcget(name=name, version=version, file='CCLE_fusions')
-    CCLE_fusions_unfiltered = tcget(name=name, version=version, file='CCLE_fusions_unfiltered')
-    CCLE_gene_cn = tcget(name=name, version=version, file='CCLE_gene_cn')
-    CCLE_mutations = tcget(name=name, version=version, file='CCLE_mutations')
-    CCLE_RNAseq_reads = tcget(name=name, version=version, file='CCLE_RNAseq_reads')
-    CCLE_RNAseq_transcripts = tcget(name=name, version=version, file='CCLE_RNAseq_transcripts')
-    CCLE_segment_cn = tcget(name=name, version=version, file='CCLE_segment_cn')
-    
-    arxspan_ids = (CCLE_expression | CCLE_expression_full | CCLE_fusions | 
-                   CCLE_fusions_unfiltered | CCLE_gene_cn | CCLE_mutations |
-                   CCLE_RNAseq_reads | CCLE_RNAseq_transcripts | CCLE_segment_cn)
+    arxspan_ids = {}
+    for file in files:
+        arxspan_ids = arxspan_ids | tcget(name=name, version=version, file=file)
+        
     return arxspan_ids
 
