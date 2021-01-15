03e20791708568a36e97ec2af5a6f110898df5ae
Javad Noorbakhsh <jnoorbak@broadinstitute.org>
2021-01-15 20:44:09 +0000

    compare files

--- depmapomics/qc/get_cell_line_set.py
+++ depmapomics/qc/get_cell_line_set.py
@@ -17,48 +17,127 @@ def tcget(name=None, version=None, file=None):
     assert all([x.startswith('ACH-') for x in arxspan_ids])    
     return arxspan_ids
 
-def get_all_arxspans(name=None, version=None, verbose=False, files=DEFAULT_FILENAMES):
-    # get the set of all the arxspans from a specific Taiga upload
-    arxspan_ids = set()
-    for file in files:
-        if verbose:
-            print('\tfetching {}'.format(file))
-        arxspan_ids = arxspan_ids | tcget(name=name, version=version, file=file)
+# def get_all_arxspans_pooled(name=None, version=None, verbose=False, files=DEFAULT_FILENAMES):
+#     # get the set of all the arxspans from a specific Taiga upload
+#     arxspan_ids = set()
+#     for file in files:
+#         if verbose:
+#             print('\tfetching {}'.format(file))
+#         arxspan_ids = arxspan_ids | tcget(name=name, version=version, file=file)
         
-    return arxspan_ids
+#     return arxspan_ids
 
-def get_arxspans_across_portals(taiga_dict, files=DEFAULT_FILENAMES, verbose=False):
-    arxpans = {}
-    for key, val in taiga_dict.items():
-        print('fetching {}/{}'.format(*val))
-        arxpans[key] = get_all_arxspans(name=val[0], version=val[1], verbose=verbose, files=files)
-    return arxpans
+def applyfunc_to_json(json_dict, func,  verbose=False):
+    arxspans = {}
+    if type(json_dict)==dict:
+        output_dict = {}
+        for k, v in json_dict.items():
+            if verbose:
+                print(k)
+            output_dict[k] = applyfunc_to_json(v, func, verbose=verbose) 
+        return output_dict
+    else:
+        return func(json_dict)
 
-def get_release_diffs(arxspans, lines_to_release, quarters, portals = ['public', 'internal', 'dmc', 'ibm']):
-    arxspan_diff = {}
-    arxspan_revdiff = {}
-    for portal in portals:
-        arxspans_expected = (arxspans[quarters[0]][portal] | set(lines_to_release[portal].dropna()))
-        arxspan_diff[portal] = arxspans[quarters[1]][portal] - arxspans_expected
-        arxspan_revdiff[portal] = arxspans_expected - arxspans[quarters[1]][portal]
-        
-    def printx(x):
-        def replace_empty(v):
-            if v == set():
-                return {'None'}
+def rename_dict(json_dict, verbose=False):
+    arxspans = {}
+    if type(json_dict)==dict:
+        output_dict = {}
+        for k, v in json_dict.items():
+            if verbose:
+                print(k)
+            if k == 'CCLE_unfiltered_fusions':
+                output_dict['CCLE_fusions_unfiltered'] = rename_dict(v, verbose=verbose) 
             else:
-                return v
+                output_dict[k] = rename_dict(v, verbose=verbose) 
+        return output_dict
+    else:
+        return json_dict    
+
+def get_release_diffs(arxspan_dict, lines_to_release, quarters = ['20q3', '21q1']):
+    release_diff = {}
+    release_diff_reverse = {}
+    for portal, portal_dict in arxspan_dict[quarters[0]].items():
+        release_diff[portal] = {}
+        release_diff_reverse[portal] = {}
+        for file in portal_dict.keys():
+            expected_lines = set(lines_to_release[portal].dropna()) | arxspan_dict[quarters[0]][portal][file]
+            
+            release_diff[portal][file] = arxspan_dict[quarters[1]][portal][file] - expected_lines
+            release_diff_reverse[portal][file] = expected_lines - arxspan_dict[quarters[1]][portal][file]
+    return release_diff, release_diff_reverse     
+
+# def get_all_arxspans(taiga_dict_expanded, verbose=False):
+#     arxspan_dict = applyfunc_to_json(taiga_dict_expanded, lambda x: tcget(name=x[0], version=x[1], file=x[2]), verbose=True)
+#     return arxspan_dict
+
+def propagate_taiga_dict_with_filenames(taiga_dict, files=DEFAULT_FILENAMES):
+    return applyfunc_to_json(taiga_dict, lambda x:{file:x+[file] for file in files})
+
+def pretty_print(release_diffs):
+    def _convert_to_text(x):
+        if x == set():
+            return 'None'
+        else:
+            return ', '.join(x)
+    text = ''
+    release_diffs_text = applyfunc_to_json(release_diffs, lambda x: _convert_to_text(x))
+    for portal, portal_dict in release_diffs_text.items():
+        text += portal + ':\n'
+        for file in portal_dict.keys():
+            text+= '\t{}: {}\n'.format(file, release_diffs_text[portal][file])
+        text+= '\n'
+    return(text)
+
+
+def pretty_print_diff(arxspan_dict, lines_to_release, quarters = ['20q3', '21q1'], savefile=False):
+    release_diffs, release_diffs_reverse = get_release_diffs(arxspan_dict, lines_to_release, quarters = quarters)
+
+    text= 'lines added ({} compared to {})\n'.format(*quarters[::-1])
+    text+= pretty_print(release_diffs)
+
+    text+= '\n'+'_'*20 + '\n'
+    text+= 'lines dropped ({} compared to {})\n'.format(*quarters[::-1])
+    text += pretty_print(release_diffs_reverse)
+    
+    if savefile:
+        with open('diff_{}_vs_{}.txt'.format(*quarters[::-1]), 'w') as f:
+            f.write(text)
+    
+    return text
+
+# def get_arxspans_across_portals_pooled(taiga_dict, files=DEFAULT_FILENAMES, verbose=False):
+#     arxpans = {}
+#     for key, val in taiga_dict.items():
+#         print('fetching {}/{}'.format(*val))
+#         arxpans[key] = get_all_arxspans_pooled(name=val[0], version=val[1], verbose=verbose, files=files)
+#     return arxpans
+
+# def get_release_diff_pooled(arxspans, lines_to_release, quarters, portals = ['public', 'internal', 'dmc', 'ibm']):
+#     arxspan_diff = {}
+#     arxspan_revdiff = {}
+#     for portal in portals:
+#         arxspans_expected = (arxspans[quarters[0]][portal] | set(lines_to_release[portal].dropna()))
+#         arxspan_diff[portal] = arxspans[quarters[1]][portal] - arxspans_expected
+#         arxspan_revdiff[portal] = arxspans_expected - arxspans[quarters[1]][portal]
         
-        if type(x)==dict:
-            text='\n'.join(['\t{}: {}'.format(k, ', '.join(replace_empty(v))) for k,v in x.items()])
-        elif type(x)==set:
-            text = ', '.join(replace_empty(x))
-        print(text)
-    print('extra arxspans per portal...')
-    printx(arxspan_diff)
-    print('\nmissing arxspans per portal...')
-    printx(arxspan_revdiff)
-    print('\nextra arxspans across portals...')
-    printx(set().union(*arxspan_diff.values()))
-    print('\nmissing arxspans across portals...')
-    printx(set().union(*arxspan_revdiff.values()))
+#     def printx(x):
+#         def replace_empty(v):
+#             if v == set():
+#                 return {'None'}
+#             else:
+#                 return v
+        
+#         if type(x)==dict:
+#             text='\n'.join(['\t{}: {}'.format(k, ', '.join(replace_empty(v))) for k,v in x.items()])
+#         elif type(x)==set:
+#             text = ', '.join(replace_empty(x))
+#         print(text)
+#     print('extra arxspans per portal...')
+#     printx(arxspan_diff)
+#     print('\nmissing arxspans per portal...')
+#     printx(arxspan_revdiff)
+#     print('\nextra arxspans across portals...')
+#     printx(set().union(*arxspan_diff.values()))
+#     print('\nmissing arxspans across portals...')
+#     printx(set().union(*arxspan_revdiff.values()))
