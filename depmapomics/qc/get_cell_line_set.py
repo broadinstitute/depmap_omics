5aa741be4c7526b5727efa894ed75eb4b239efab
Javad Noorbakhsh <jnoorbak@broadinstitute.org>
2021-01-15 01:28:45 +0000

    get arxspans across portals

--- depmapomics/qc/get_cell_line_set.py
+++ depmapomics/qc/get_cell_line_set.py
@@ -1,6 +1,10 @@
 from taigapy import TaigaClient
 tc = TaigaClient()
 
+DEFAULT_FILENAMES=['CCLE_expression', 'CCLE_expression_full', 'CCLE_fusions',
+                   'CCLE_fusions_unfiltered', 'CCLE_gene_cn', 'CCLE_mutations',
+                   'CCLE_RNAseq_reads', 'CCLE_RNAseq_transcripts', 'CCLE_segment_cn']
+
 def tcget(name=None, version=None, file=None):
     # get arxspan ids from the Taiga file 
     # this function avoids overloading the memory because the entire variable is local to it
@@ -13,10 +17,7 @@ def tcget(name=None, version=None, file=None):
     assert all([x.startswith('ACH-') for x in arxspan_ids])    
     return arxspan_ids
 
-def get_all_arxspans(name=None, version=None, verbose=False,
-        files=['CCLE_expression', 'CCLE_expression_full', 'CCLE_fusions',
-               'CCLE_fusions_unfiltered', 'CCLE_gene_cn', 'CCLE_mutations',
-               'CCLE_RNAseq_reads', 'CCLE_RNAseq_transcripts', 'CCLE_segment_cn']):
+def get_all_arxspans(name=None, version=None, verbose=False, files=DEFAULT_FILENAMES):
     # get the set of all the arxspans from a specific Taiga upload
     arxspan_ids = set()
     for file in files:
@@ -25,3 +26,10 @@ def get_all_arxspans(name=None, version=None, verbose=False,
         arxspan_ids = arxspan_ids | tcget(name=name, version=version, file=file)
         
     return arxspan_ids
+
+def get_arxspans_across_portals(taiga_dict, files=DEFAULT_FILENAMES, verbose=False):
+    arxpans = {}
+    for key, val in taiga_dict.items():
+        print('fetching {}/{}'.format(*val))
+        arxpans[key] = get_all_arxspans(name=val[0], version=val[1], verbose=verbose, files=files)
+    return arxpans
