c8a9df20555f90df5ea170b48faa3a79f81943b1
Javad Noorbakhsh <jnoorbak@broadinstitute.org>
2021-01-19 17:49:13 +0000

    create tests

---
+++ depmapomics/qc/test/test_gene_cn_against_old_version.py
@@ -0,0 +1,6 @@
+from depmapomics.qc.compare_cn import plot_gene_cn_comparison
+
+%%time
+gene_cn_20q3_21q1 = plot_gene_cn_comparison(release1 = {'name': 'internal-20q3-00d0', 'version': 9},
+                                            release2 = {'name': 'internal-21q1-4fc4', 'version': 7})
+
