dce71da2e1db15b8a3a93ed35ebd564fd205813a
Javad Noorbakhsh <jnoorbak@broadinstitute.org>
2021-02-18 15:06:57 -0500

    lint

--- depmapomics/helper.py
+++ depmapomics/helper.py
@@ -1,19 +1,21 @@
 from genepy import rna
 from depmapomics.config import RNASEQC_THRESHOLDS_LOWQUAL, RNASEQC_THRESHOLDS_FAILED
+from src.CCLE_postp_function import getQC
 
 import pandas as pd
 
-def plot_rnaseqc_results(workspace, samplelist, output_path = 'data/rna_qc_plots/'):
+def plot_rnaseqc_results(workspace, samplelist, output_path='data/rna_qc_plots/'):
     rnaqc = getQC(workspace=workspace, only=samplelist, qcname="rnaseqc2_metrics")
     assert pd.Series(rnaqc).map(lambda x: x[0]).notnull().all()
 
     import numpy as np
     qcs = pd.DataFrame()
-    for k,val in rnaqc.items():
+    for k, val in rnaqc.items():
         if val[0] is not np.nan:
-            qcs = pd.concat([qcs, pd.read_csv(val[0],sep='\t', index_col=0)],axis=1)
-    qcs = qcs[~((qcs.mean(1)==1.0) | (qcs.mean(1)==0.0))]
+            qcs = pd.concat([qcs, pd.read_csv(val[0], sep='\t', index_col=0)], axis=1)
+    qcs = qcs[~((qcs.mean(1) == 1.0) | (qcs.mean(1) == 0.0))]
 
+    # pandas implementation
     # qcs = pd.Series(rnaqc).map(lambda x: x[0]).dropna()
     # qcs = qcs.apply(lambda x: pd.read_csv(x, sep='\t', index_col=0))
     # qcs = pd.concat(qcs.tolist(), axis=1)
