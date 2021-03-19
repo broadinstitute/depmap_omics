e14fd7bf5879cf8ec53f584a076be1c27707a566
jkobject <jkobject@gmail.com>
2021-03-19 14:36:22 +0000

    renaming folder

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='depmapomics',
       description='Pipelines for the analysis of the DepMap omics data',
       url='https://github.com/broadinstitute/ccle_processing',
       # install_requires=open("requirements.txt").readlines(),
-      packages=['src']
+      packages=['depmapomics']
       )
 # add requiremnts
 
