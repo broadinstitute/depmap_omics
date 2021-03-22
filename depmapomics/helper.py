e8ff8756548ae79679536556da66900543b25ad4
Javad Noorbakhsh <jnoorbak@broadinstitute.org>
2021-03-22 13:04:02 -0400

    inherit from igv Browser

--- depmapomics/helper.py
+++ depmapomics/helper.py
@@ -3,6 +3,8 @@ import sys
 import pandas as pd
 from genepy import rna
 from src.CCLE_postp_function import getQC
+import igv
+from genepy.utils.helper import parrun
 
 from depmapomics.config import (RNASEQC_THRESHOLDS_FAILED,
                                 RNASEQC_THRESHOLDS_LOWQUAL)
@@ -38,3 +40,31 @@ def plot_rnaseqc_results(workspace, samplelist, output_path='data/rna_qc_plots/'
                                  qant1=0.07, qant3=0.93)
 
     return qcs, lowqual, failed
+
+
+def get_gcloud_auth_token():
+    parrun(["echo $(gcloud auth application-default print-access-token) > /tmp/gcloud_token"], cores=1)
+
+    with open('/tmp/gcloud_token', 'r') as f:
+        token = f.readline().strip()
+    return token
+
+class depmap_igv(igv.Browser):
+    def __init__(self, config):
+        super().__init__({**config, "oauthToken": get_gcloud_auth_token()})
+
+    def get_the_track(self, depmap_sample, datatype, iloc_value=0, column_type='legacy', fileformat='bam'):
+        sample_info = depmap_sample[depmap_sample['datatype'] == datatype].iloc[iloc_value]
+        self.load_track(
+        {
+            "name": '{} ({}): {}'.format(sample_info['arxspan_id'],
+                                         sample_info['stripped_cell_line_name'],
+                                         sample_info['datatype']),
+            "url": sample_info['{}_bam_filepath'.format(column_type)],
+            'indexURL': sample_info['{}_bai_filepath'.format(column_type)],
+            "format": fileformat
+        })
+
+    def get_the_tracks(self, depmap_sample, schemas):
+        for schema in schemas:
+            self.get_the_track(depmap_sample, **schema)
