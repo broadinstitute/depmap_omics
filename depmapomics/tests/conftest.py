e8b739cae486eba933fed7d7e7f2375f4a80ea51
Javad Noorbakhsh <jnoorbak@broadinstitute.org>
2021-04-22 18:41:41 -0400

    minor

--- depmapomics/tests/conftest.py
+++ depmapomics/tests/conftest.py
@@ -1,9 +1,5 @@
 from depmapomics.tests.config import VIRTUAL_RELEASE, REFERENCE_RELEASE
 
 def pytest_sessionstart(session):
-    """
-    Called after the Session object has been created and
-    before performing collection and entering the run test loop.
-    """
     print('REFERENCE_RELEASE: ', REFERENCE_RELEASE)
     print('VIRTUAL_RELEASE: ', VIRTUAL_RELEASE)
