1e1a43d17ff39e7554a1c81548c61f553de3ae4a
Jérémie Kalfon <jkobject@gmail.com>
2021-04-28 09:41:40 -0400

    adding a setup and some  comments

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setup(name='depmapomics',
       description='Pipelines for the analysis of the DepMap omics data',
-      url='https://github.com/broadinstitute/ccle_processing',
-      # install_requires=open("requirements.txt").readlines(),
-      packages=['depmapomics']
+      url='https://github.com/broadinstitute/depmapomics',
+      install_requires=open("requirements.txt").readlines(),
+      packages=['depmapomics'],
+      long_description=long_description,
+      author='Jeremie Kalfon',
+      author_email='jkobject@gmail.com',
+      python_requires='>=3.8',
+      package_data={'depmapomics': ['data/*']},
       )
