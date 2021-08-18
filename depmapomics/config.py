5cff679853764e55d65d1bd10ef18dc7142a989e
Jérémie Kalfon <jkobject@gmail.com>
2021-08-18 15:24:45 -0400

    using env too

--- depmapomics/config.py
+++ depmapomics/config.py
@@ -1,3 +1,5 @@
+import os.getenv
+
 def fileToList(filename):
   """
   loads an input file with a\\n b\\n.. into a list [a,b,..]
@@ -6,7 +8,7 @@ def fileToList(filename):
     return [val[:-1] for val in f.readlines()]
 
 
-if fileToList("../.git/HEAD")[0].split('/')[-1] == "master":
+if fileToList("../.git/HEAD")[0].split('/')[-1] == "master" or os.getenv('DEPMAP_ENV') == "PROD":
   from depmapomics.config_prod import *
 else:
   from depmapomics.config_dev import *
