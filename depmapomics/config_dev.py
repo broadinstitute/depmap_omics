e992321ef5d6e20187b067f308645c3a3f929d88
Jérémie Kalfon <jkobject@gmail.com>
2021-09-03 15:59:07 -0400

    reworking structure of prod config file and adding some more doc

--- depmapomics/config_dev.py
+++ depmapomics/config_dev.py
@@ -285,10 +285,6 @@ FUSION_RED_HERRING = ['GTEx_recurrent', 'DGD_PARALOGS', 'HGNC_GENEFAM',
 
 ############## EXPRESSION
 
-## genomic annotations (v35)
-GENCODE = 'ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_35/gencode.v35.annotation.gff3.gz'
-
-
 BAM_GCS_BUCKET = 'gs://cclebams-sandbox'
 
 RNAGSPATH38 = BAM_GCS_BUCKET + "/rnasq_hg38/"
