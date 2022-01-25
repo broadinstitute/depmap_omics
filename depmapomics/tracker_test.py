07ec20d86a3a8edd9aacbbd8d3799a494e2a79a9
Simone <xiaomeng@broadinstitute.org>
2022-01-25 17:54:17 +0000

    test script wip

--- depmapomics/tracker_test.py
+++ depmapomics/tracker_test.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import numpy as np
+from depmapomics.config import *
 from depmapomics import tracker as track
+from depmapomics import expressions
 
 
 def test_expression_outputs():
+    trackerobj = track.initTracker()
     testset_name = "test_postprocessing"
+
+    updated_tracker = expressions._CCLEPostProcessing(
+        samplesetname=SAMPLESETNAME,
+        trackerobj=trackerobj,
+        samplesetToLoad=testset_name,
+        dry_run=True,
+    )
 
     return True
