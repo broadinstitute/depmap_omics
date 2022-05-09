3eb49c028fcf688c0d92320a3121dc9e28db739f
Simone <xiaomeng@broadinstitute.org>
2022-05-09 20:25:02 +0000

    achilles table func wip

--- depmapomics/upload.py
+++ depmapomics/upload.py
@@ -32,8 +32,82 @@ def getPRToRelease(trackerobj):
     return prs
 
 
-def makeAchillesChoiceTable():
-    return True
+def makeAchillesChoiceTable(
+    trackerobj, one_pr_per_type=True, taiga_ids=VIRTUAL, folder="temp/" + SAMPLESETNAME
+):
+    """generate and upload a table for each portal that indicates which profiles are released corresponding to which MC
+
+    Args:
+        trackerobj (SampleTracker): tracker object
+        taiga_ids (dict): dictionary that maps portal name to virtual taiga dataset id
+        folder (str): location where tables are saved as .csv files
+
+    Returns:
+        prs (dict{(str: pd.DataFrame)}): for each portal, list of profile IDs
+    """
+    avail_prs = getPRToRelease(trackerobj)
+    pr_table = trackerobj.read_pr_table()
+    seq_table = trackerobj.read_seq_table()
+    ach_tables = dict()
+    for k, v in avail_prs:
+        rows = []
+        subset_pr_table = pr_table.loc[v]
+        mcs = set(subset_pr_table["ModelCondition"])
+        # one_pr_per_type assumes we're only picking one PR per datatype (rna/dna) for each MC
+        if one_pr_per_type:
+            for mc in mcs:
+                prs_in_mc = subset_pr_table[(subset_pr_table.ModelCondition == mc)]
+                # rna
+                if len(prs_in_mc[prs_in_mc.Datatype == "rna"]) == 1:
+                    pr = prs_in_mc[prs_in_mc.Datatype == "rna"].ProfileID
+                    rows.append((mc, pr, "rna"))
+                elif len(prs_in_mc[prs_in_mc.Datatype == "rna"]) > 1:
+                    cds_ids = prs_in_mc[prs_in_mc.Datatype == "rna"].CDSID.tolist()
+                    latest_cds_id = seq_table.loc[cds_ids, "version"].idxmax()
+                    pr = subset_pr_table[subset_pr_table.CDSID == latest_cds_id]
+                    rows.append((mc, pr, "rna"))
+                # dna
+                if (
+                    len(
+                        prs_in_mc[
+                            (prs_in_mc.Datatype == "wgs")
+                            | (prs_in_mc.Datatype == "wes")
+                        ]
+                    )
+                    == 1
+                ):
+                    pr = prs_in_mc[
+                        (prs_in_mc.Datatype == "wgs") | (prs_in_mc.Datatype == "wes")
+                    ].ProfileID
+                    rows.append((mc, pr, "dna"))
+                elif (
+                    len(
+                        prs_in_mc[
+                            (prs_in_mc.Datatype == "wgs")
+                            | (prs_in_mc.Datatype == "wes")
+                        ]
+                    )
+                    > 1
+                ):
+                    cds_ids_wgs = prs_in_mc[prs_in_mc.Datatype == "wgs"].CDSID.tolist()
+                    cds_ids_wes = prs_in_mc[prs_in_mc.Datatype == "wes"].CDSID.tolist()
+                    pr = ""
+                    if len(cds_ids_wgs) == 0:
+                        latest_cds_id_wes = seq_table.loc[
+                            cds_ids_wes, "version"
+                        ].idxmax()
+                        pr = subset_pr_table[subset_pr_table.CDSID == latest_cds_id_wes]
+                    else:
+                        latest_cds_id_wgs = seq_table.loc[
+                            cds_ids_wgs, "version"
+                        ].idxmax()
+                        pr = subset_pr_table[subset_pr_table.CDSID == latest_cds_id_wgs]
+                    rows.append((mc, pr, "dna"))
+        ach_tables[k] = pd.DataFrame(
+            rows, columns=["ModelConditionID", "ProfileID", "ProfileType"]
+        )
+
+    return ach_tables
 
 
 def makeDefaultModelTable():
