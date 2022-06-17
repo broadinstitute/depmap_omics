d6200223e4236560bd732491d05b163781d0774f
Simone <xiaomeng@broadinstitute.org>
2022-06-17 19:50:02 +0000

    address pylint

--- depmapomics/qc/create_tentative_virtual.py
+++ depmapomics/qc/create_tentative_virtual.py
@@ -4,8 +4,10 @@ from depmapomics.qc.config import TENTATIVE_VIRTUAL, TAIGA_IDS_LATEST
 
 def get_taiga_id_with_version(taiga_id):
     tc = TaigaClient()
-    latest_version = max([int(x['name']) for x in tc.get_dataset_metadata(taiga_id)['versions']])
-    taiga_id_with_version = '{}.{}'.format(taiga_id, latest_version)
+    latest_version = max(
+        int(x["name"]) for x in tc.get_dataset_metadata(taiga_id)["versions"]
+    )
+    taiga_id_with_version = "{}.{}".format(taiga_id, latest_version)
     return taiga_id_with_version
 
 
@@ -14,10 +16,12 @@ def get_taiga_ids_list(taiga_id_dict):
     for taiga_id, pairs in taiga_id_dict.items():
         taiga_id_with_version = get_taiga_id_with_version(taiga_id)
         for pair in pairs:
-            taiga_ids_list.append({
-                'name': pair[0],
-                'taiga_id': '{}/{}'.format(taiga_id_with_version, pair[1])
-            })
+            taiga_ids_list.append(
+                {
+                    "name": pair[0],
+                    "taiga_id": "{}/{}".format(taiga_id_with_version, pair[1]),
+                }
+            )
     return taiga_ids_list
 
 
@@ -40,14 +44,13 @@ def update_tentative_virtual():
     #     folder_id="29b48277847443edb7d84b9b457de124", # optional, will default to your home folder if not provided
     # )
 
-
     taiga_ids_list = get_taiga_ids_list(TAIGA_IDS_LATEST)
 
     new_dataset_id = tc.update_dataset(
-        TENTATIVE_VIRTUAL['name'],
+        TENTATIVE_VIRTUAL["name"],
         changes_description="add all the virtual data",
         add_taiga_ids=taiga_ids_list,
-        add_all_existing_files = False
+        add_all_existing_files=False,
     )
 
 
