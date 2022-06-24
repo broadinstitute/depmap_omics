101b82deb5efe6fe05fe72c2d4ee28fdbc3d66a8
Simone <xiaomeng@broadinstitute.org>
2022-06-24 15:33:25 +0000

    delete CHROMLIST

--- depmapomics/config_global.py
+++ depmapomics/config_global.py
@@ -59,33 +59,6 @@ filetypes = ["bam", "bai"]
 
 MATCH = ["ACH-", "CDS-"]
 
-# chromosome list
-CHROMLIST = [
-    "chr1",
-    "chr2",
-    "chr3",
-    "chr4",
-    "chr5",
-    "chr6",
-    "chr7",
-    "chr8",
-    "chr9",
-    "chr10",
-    "chr11",
-    "chr12",
-    "chr13",
-    "chr14",
-    "chr15",
-    "chr16",
-    "chr17",
-    "chr18",
-    "chr19",
-    "chr20",
-    "chr21",
-    "chr22",
-    "chrX",
-]
-
 # default values in the GP workspaces and our sample tracker (to change if you use another workspace/
 # sample tracker)
 EXTRACT_DEFAULTS = {
