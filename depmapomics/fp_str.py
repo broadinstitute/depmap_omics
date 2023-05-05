05b81ffe3942ea2062f8239be39c6b1092aa3941
Simone <xiaomeng@broadinstitute.org>
2023-05-05 19:41:31 +0000

    move str functions from ipynb to its own module

---
+++ depmapomics/fp_str.py
@@ -0,0 +1,141 @@
+import io
+import os
+import pandas as pd
+import gzip
+import numpy as np
+from depmapomics import constants
+from depmap_omics_upload import tracker as track
+
+from google.cloud import storage  # TODO: look for alternatives
+
+
+def read_vcf(path):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(path.split("/")[2])
+    blob = bucket.blob("/".join(path.split("/")[3:]))
+    if path.endswith(".gz"):
+        data = io.BytesIO(blob.download_as_string())
+        with gzip.open(data, "r") as f:
+            lines = [l.decode("utf-8") for l in f if not l.startswith(b"#")]
+    else:
+        data = blob.download_as_string().decode("utf-8")
+        f = data.split("\n")
+        lines = [l + "\n" for l in f if not l.startswith("#")]
+    return pd.read_csv(
+        io.StringIO("".join(lines)),
+        names=[
+            "CHROM",
+            "POS",
+            "ID",
+            "REF",
+            "ALT",
+            "QUAL",
+            "FILTER",
+            "INFO",
+            "FORMAT",
+            "GT",
+        ],
+        sep="\t",
+    )
+
+
+def transformGB(row):
+    if len(row) < 2:
+        return np.nan
+    else:
+        split_gt = row.split(":")[1].split("|")
+        return [int(i) for i in split_gt]
+
+
+def gb2str(row):
+    if not isinstance(row["GB"], list):
+        return "NA"
+    else:
+        strs = []
+        for i in row["GB"]:
+            integer = (row["REF"] * row["PERIOD"] + i) // row["PERIOD"]
+            remainder = (row["REF"] * row["PERIOD"] + i) % row["PERIOD"]
+            if remainder == 0:
+                strs.append(str(int(integer)))
+            else:
+                strs.append(str(int(integer)) + "." + str(int(remainder)))
+        return ", ".join(list(set(strs)))
+
+
+def altAllele2str(row):
+    if pd.isna(row["ALT"]) or row["ALT"] == ".":
+        return "NA"
+    else:
+        strs = []
+        for i in row["ALT"].split(","):
+            l = len(i)
+            integer = l // row["PERIOD"]
+            remainder = l % row["PERIOD"]
+            if remainder == 0:
+                strs.append(str(int(integer)))
+            else:
+                strs.append(str(int(integer)) + "." + str(int(remainder)))
+        return ", ".join(list(set(strs)))
+
+
+def generateSTRRow(
+    paths_df, method, str_bed=constants.STR_BED, colname=constants.STR_COLNAME
+):
+    hg38_sites = pd.read_csv(
+        str_bed,
+        sep="\t",
+        names=["CHROM", "START", "END", "PERIOD", "REF", "ID"],
+    ).astype({"PERIOD": "int32"})
+    str_rows = []
+    for i, p in paths_df.iterrows():
+        if pd.isna(p[colname]):
+            print("no hipSTR vcf available for: " + i)
+        else:
+            df = read_vcf(p[colname])
+            if method == "hipstr":
+                df["GB"] = df.apply(lambda x: (transformGB(x["GT"])), axis=1)
+                df = hg38_sites.merge(df[["ID", "GB"]], on="ID", how="left")
+                df["STR"] = df.apply(lambda x: gb2str(x), axis=1)
+            elif method == "gangstr":
+                df = df.rename(columns={"POS": "START"})
+                df = hg38_sites.merge(
+                    df[["CHROM", "START", "ALT"]], on=["CHROM", "START"], how="left"
+                )
+                df["STR"] = df.apply(lambda x: altAllele2str(x), axis=1)
+            # Maybe no need to convert to model IDs here yet?
+            df["sample_id"] = i
+            str_row = df.pivot(index="sample_id", columns="ID", values="STR")
+            str_rows.append(str_row)
+    return pd.concat(str_rows)
+
+
+def computeTanabe(df1, idx1, df2, idx2, colnames=constants.STR_LOCATIONS_13):
+    # compute tanabe similarity between two STR profiles
+    match = 0
+    total = 0
+    for col in colnames:
+        # TODO: how to best handle NAs?
+        a1 = set(df1.loc[idx1, col].split(", "))
+        a2 = set(df2.loc[idx2, col].split(", "))
+        if a1 != set(["NA"]) & a2 != set(["NA"]):
+            match += len(set(a1) & set(a2))
+            total += len(set(a1)) + len(set(a2))
+    return 2 * match / total
+
+
+def makeScoreMatrixDatabase(df_seqid, df_achid, colnames=constants.STR_LOCATIONS_13):
+    mytracker = track.SampleTracker()
+    seq_table = mytracker.add_model_cols_to_seqtable(cols=[constants.MODEL_TABLE_INDEX])
+    valid_achids = list(set(df_achid.index) - set([np.nan]))
+    scoremat = pd.DataFrame(
+        columns=valid_achids + [constants.MODEL_TABLE_INDEX], index=(df_seqid.index)
+    )
+    for i in df_seqid.index:
+        scoremat.loc[i, constants.MODEL_TABLE_INDEX] = seq_table.loc[
+            i, constants.MODEL_TABLE_INDEX
+        ]
+        for j in valid_achids:
+            scoremat.loc[i, j] = computeTanabe(
+                df_seqid, i, df_achid, j, colnames=colnames
+            )
+    return scoremat
