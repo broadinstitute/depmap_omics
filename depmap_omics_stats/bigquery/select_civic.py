2b4dc4d138202a7056818a150575182ad810a7b3
Alvin Qin <qqin@broadinstitute.org>
2023-05-27 22:27:29 -0400

    add python bigquery example for each sample

--- depmap_omics_stats/bigquery/select_civic.py
+++ depmap_omics_stats/bigquery/select_civic.py
@@ -1,9 +1,11 @@
 from google.cloud import bigquery
 from google.oauth2 import service_account
+import pandas as pd
 
 credentials = service_account.Credentials.from_service_account_file('/Users/qqin/.sparkles-cache/service-keys/broad-qqin.json')
 project_id = 'depmap-omics'
 client = bigquery.Client(credentials= credentials,project=project_id)
+
 
 def job_query(sql):
     query_job = client.query(sql)
@@ -12,8 +14,11 @@ def job_query(sql):
 
 
 def main():
-    df = job_query("""SELECT COUNTIF(civic_score > 0) AS CIVIC_thresh0, COUNTIF(civic_score >8) AS CIVIC_thresh8, COUNTIF(civic_score >20) as CIVIC_thresh20 FROM `depmap-omics.maf_staging.merged_maf`""")
+    df = job_query("""SELECT COUNTIF(civic_score > 0) AS CIVIC_thresh0, COUNTIF(civic_score > 5) AS CIVIC_thresh5, COUNTIF(civic_score > 8) AS CIVIC_thresh8, COUNTIF(civic_score >20) as CIVIC_thresh20 FROM `depmap-omics.maf_staging.merged_maf`""")
     print(df)
+    df_samples = job_query("""SELECT COUNTIF(civic_score > 0) AS CIVIC_thresh0, COUNTIF(civic_score > 5) AS CIVIC_thresh5, COUNTIF(civic_score > 8) AS CIVIC_thresh8, COUNTIF(civic_score >20) as CIVIC_thresh20 FROM `depmap-omics.maf_staging.merged_maf` GROUP BY CDS_ID""")
+    df = pd.concat([df, df_samples], axis=0)
+    df.to_csv("bigquery_civic.csv")
 
 
 if __name__ == "__main__":
