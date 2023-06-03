ef69db55a55178630e03b7b363e3216644f29735
qinqian <qqin@broadinstitute.org>
2023-06-03 18:09:46 +0000

    add pytest for vcf2maf, not working yet

---
+++ tests/depmapomics/tasks/test_vcf2maf.py
@@ -0,0 +1,33 @@
+from .utils import run_wdl, assert_output_dirs_match
+
+# The goal of this testing approach is to make sure that the "plumbing" between is correct. This is basically an
+# integration test which can detect regressions (changes from previous behavior), but nothing more than that. While
+# not an especially robust testing approach, it is much better than nothing.
+
+
+# Since these pipelines are run on large inputs, it'll be better to keep the data in cloud storage, rather
+# than checked into git.
+#
+# Unfortunately, this means that we lose the ability to keep the test data in sync with the code in the git repository.
+# It will be confusing if, in updating a test, we change an input file, causing the test to fail in older versions.
+# However, we can avoid this if we
+#
+# put all GCS paths as constants at the top here with the suffix "GCS_PATH". If we ever need to go clean up the
+# bucket, it'll be easier if we can grep out all of the paths out of the code.
+SUBSET_VCF_GCS_PATH = "gs://depmapomics-testdata/inputs/230519/CDS-R99tiF.subset.vcf.gz"
+VCS_TO_DEPMAP_EXPECTED_GCS_PATH = (
+    "gs://depmapomics-testdata/outputs/230519/test_vcs_to_depmap/expected"
+)
+
+
+def test_vcf_to_maf(tmpdir):
+    run_wdl(
+        tmpdir,
+        "sandbox/vcf2maf/vcf2maf.wdl",
+        {"input_vcf": SUBSET_VCF_GCS_PATH, "sample_id": "CDS-R99tiF"},
+        "test_vcf2maf/latest",
+    )
+
+    assert_output_dirs_match(
+        VCS_TO_DEPMAP_EXPECTED_GCS_PATH, "test_vcs_to_depmap/latest"
+    )
