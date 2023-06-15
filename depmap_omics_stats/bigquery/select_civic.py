bd0e848f559d24a1732c1d5641ce5c9896557194
qinqian <qqin@broadinstitute.org>
2023-06-15 18:55:08 +0000

    add whitelisting query

--- depmap_omics_stats/bigquery/select_civic.py
+++ depmap_omics_stats/bigquery/select_civic.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from google.cloud import bigquery
 from google.oauth2 import service_account
 import pandas as pd
@@ -6,49 +8,82 @@ from matplotlib import pyplot as plt
 #from sklearn.tree import DecisionTreeClassifier
 #from sklearn import tree
 
-
 credentials = service_account.Credentials.from_service_account_file('broad-qqin.json')
 project_id = 'depmap-omics'
 client = bigquery.Client(credentials= credentials,project=project_id)
 
 
-def job_query(sql):
+def job_query_to_dataframe(sql):
     query_job = client.query(sql)
     df = query_job.to_dataframe()
     return df
 
 
-def main():
-    df_civic = job_query("""SELECT COUNTIF(civic_score >= 8) AS CIVIC_thresh8 FROM `depmap-omics.maf_staging.merged_maf`""")
-    #df_civic_samples = job_query("""SELECT COUNTIF(civic_score > 0) AS CIVIC_thresh0, COUNTIF(civic_score > 5) AS CIVIC_thresh5, COUNTIF(civic_score > 8) AS CIVIC_thresh8, COUNTIF(civic_score >20) as CIVIC_thresh20 FROM `depmap-omics.maf_staging.merged_maf` GROUP BY CDS_ID""")
-
-    df_pon = job_query("""SELECT COUNTIF(panel_of_normals!='') AS pon FROM `depmap-omics.maf_staging.merged_maf`""")
-    #df_pon_samples = job_query("""SELECT COUNTIF(panel_of_normals='') AS pon FROM `depmap-omics.maf_staging.merged_maf` GROUP BY CDS_ID""")
-    print(df_pon)
-
-    df_af = job_query("""SELECT COUNTIF(CAST(SPLIT(af, ',')[ORDINAL(1)] AS NUMERIC) >= 0.15) AS af FROM `depmap-omics.maf_staging.merged_maf`""")
-
-    df_dp = job_query("""SELECT COUNTIF(CAST(SPLIT(dp, ',')[ORDINAL(1)] AS NUMERIC) >= 2) AS dp FROM `depmap-omics.maf_staging.merged_maf`""")
-
-    df_dann = job_query("""SELECT COUNTIF(SAFE_CAST(dann_score AS NUMERIC) >= 0.96) AS dann FROM `depmap-omics.maf_staging.merged_maf`""")
-
-    df_gnomad3 = job_query("""SELECT COUNTIF(SAFE_CAST(popaf AS NUMERIC) >= 3) AS gnomad3 FROM `depmap-omics.maf_staging.merged_maf`""")
-
-    df_gnomad5 = job_query("""SELECT COUNTIF(SAFE_CAST(popaf AS NUMERIC) >= 5) AS gnomad5 FROM `depmap-omics.maf_staging.merged_maf`""")
-
-    df_all = job_query("""SELECT table_id, row_count, size_bytes
+def query_all_variants():
+    df_all = job_query_to_dataframe("""SELECT table_id, row_count, size_bytes
     FROM `depmap-omics.maf_staging.__TABLES__`
     WHERE STARTS_WITH(table_id, "merged")
     ORDER BY table_id DESC""")
+    return df_all
 
+
+def filter_fields_query(fields=['PASS', 'multiallelic', 'af', 'dp', 'is_coding', 'germline', 'pon', 'popaf'], 
+                        cutoffs=['Y', 'Y', 0.15, 2, 'Y', 'Y', 'Y', 5],
+                        groupby=None):
+    if groupby is not None:
+        group_sql = " GROUP BY CDS_ID "
+    else:
+        group_sql = ""
+
+    dfs = []
+    for field, cutoff in zip(fields, cutoffs):
+        print(field,cutoff)
+        if isinstance(cutoff, str):
+            df = job_query_to_dataframe(f"""SELECT COUNTIF({field}='{cutoff}') AS {field} FROM `depmap-omics.maf_staging.merged_maf` {group_sql}""")
+        else:
+            df = job_query_to_dataframe(f"""SELECT COUNTIF(SAFE_CAST(SPLIT({field}, ',')[ORDINAL(1)] AS NUMERIC) >= {cutoff}) AS {field} FROM `depmap-omics.maf_staging.merged_maf` {group_sql}""")
+        dfs.append([f"{field}:{cutoff}", df.values[0][0]])
+    return dfs
+
+
+def whitelisting_fields_query(fields=['civic_score', 'civic_score', 'brca1_func_score', 'dann_score', 'hess_driver', 'likely_driver', 'likely_lof'], 
+                              cutoffs=[1, 8, -1.328, 0.96, 'Y', 'Y', 'Y'],
+                              groupby=None):
+    if groupby is not None:
+        group_sql = " GROUP BY CDS_ID "
+    else:
+        group_sql = ""
+
+    dfs = []
+    for field, cutoff in zip(fields, cutoffs):
+        print(field,cutoff)
+        if isinstance(cutoff, str):
+            df = job_query_to_dataframe(f"""SELECT COUNTIF({field}='{cutoff}') AS {field} FROM `depmap-omics.maf_staging.merged_maf` {group_sql}""")
+        elif field == 'civic_score':
+            df = job_query_to_dataframe(f"""SELECT COUNTIF({field} >= {cutoff}) AS {field} FROM `depmap-omics.maf_staging.merged_maf` {group_sql}""")
+        elif field=='brca1_func_score':
+            df = job_query_to_dataframe(f"""SELECT COUNTIF(SAFE_CAST(SPLIT({field}, ',')[ORDINAL(1)] AS NUMERIC) <= {cutoff}) AS {field} FROM `depmap-omics.maf_staging.merged_maf` {group_sql}""")
+        else:
+            df = job_query_to_dataframe(f"""SELECT COUNTIF(SAFE_CAST(SPLIT({field}, ',')[ORDINAL(1)] AS NUMERIC) >= {cutoff}) AS {field} FROM `depmap-omics.maf_staging.merged_maf` {group_sql}""")
+        dfs.append([f"{field}:{cutoff}", df.values[0][0]])
+    return dfs
+
+
+def main():
+    df_filter = filter_fields_query()
+    df_whitelist = whitelisting_fields_query()
+    print(df_filter)
+    print(df_whitelist)
+    df_all = query_all_variants()
     print(df_all)
-    #df = pd.concat([df_civic, df_civic_samples, af], axis=0)
-    df = pd.concat([df_civic, df_pon, df_af, df_dp, df_dann, df_gnomad3, df_gnomad5], axis=0)
-    print(df)
-    print(df/df_all.values.flatten()[1])
-    df.to_csv("bigquery_civic.csv")
+    df_filter = pd.DataFrame(df_filter)
+    df_whitelist = pd.DataFrame(df_whitelist)
+    df_filter.iloc[:, 1] /= df_all.row_count.values[0]
+    df_whitelist.iloc[:, 1] /= df_all.row_count.values[0]
+    print(df_filter)
+    print(df_whitelist)
+    df_all.to_csv("bigquery_civic.csv")
 
 
 if __name__ == "__main__":
     main()
-
