07ce7b777fbad8928123d90a6edb817a103eefbc
5im1z <xiaomeng@broadinstitute.org>
2023-06-16 21:27:20 +0000

    some notes

--- sandbox/echtvar/cosmic/run.sh
+++ sandbox/echtvar/cosmic/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -ex
 
-python3 process_cosmic_data.py CosmicCodingMuts.hg38.normal.vcf.gz CosmicMutantExport_hg38.tsv.gz
+# only need to run once (now saved in google bucket):
+# python3 process_cosmic_data.py CosmicCodingMuts.hg38.normal.vcf.gz CosmicMutantExport_hg38.tsv.gz
 
-~/bin/echtvar encode \
-            cosmic_coding_ev.zip \
-            cosmic.json \
-            CosmicCodingMuts.hg38.normal_GRCh38_processed.vcf.gz
+# ~/bin/echtvar encode \
+#             cosmic_coding_ev.zip \
+#             cosmic.json \
+#             CosmicCodingMuts.hg38.normal_GRCh38_processed.vcf.gz
 
 bcftools norm -m- /home/ubuntu/depmap_omics/CDS-9ImTq5.filtered.vcf.gz -w 10000 -f /home/ubuntu/depmap_omics/Homo_sapiens_assembly38.fasta -O b -o ~/CDS-9ImTq5.hg38-unfiltered.bcf
 
@@ -17,4 +18,7 @@ bcftools norm -m- /home/ubuntu/depmap_omics/CDS-9ImTq5.filtered.vcf.gz -w 10000 
 # 2. make a docker file
 # 3. build a docker image
 # 4. make a wdl file that optionally uses this docker image
-# 5. write a test in the style of 
+# 5. write a test in the style of branch pgm-sample-test
+# 6. poetry install
+# 7. set python env in vscode
+# 8. run pytest
