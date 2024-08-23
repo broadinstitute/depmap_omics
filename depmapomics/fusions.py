6195c738294271ed3412b7294fe5ac6947a1c4a6
Lauren Golden <golden@broadinstitute.org>
2024-08-23 13:21:34 -0400

    dropped string columns that arent used by depmap omics fusions calls and prevented uploading to taiga

--- depmapomics/fusions.py
+++ depmapomics/fusions.py
@@ -171,44 +171,6 @@ def postProcess(
         (pd.df): filtered fusion dataframe
     """
 
-    """
-R code for aggregating fusions:
-library(plyr)
-library(dplyr)
-library(readr)
-library(stringr)
-
-## reads selected fields from maf files and aggregates them 
-args <- commandArgs(TRUE)
-print(args)
-
-output_file_name = args[1]
-input_file_names = args[2]
-
-# Load files and ensure that we are only reading in files that exist
-fns = as.character(read.csv(input_file_names, header=FALSE)[,])
-fe = sapply(fns, file.exists)
-samples = fns[which(fe)]
-nsamples = length(samples)
-
-# Iterate across samples, read tsv, add the DepMap_ID column and keep the rest of them
-for(i in 1:nsamples){
-  f <- samples[i]
-  
-  # Extract the arxspan id from the file name
-  # sample <- stringr::str_extract(string = f, pattern = 'ACH\\-[0-9]+')
-  
-  # In this workspace, samples are not indexed by ARXSPAND ID but by CCLE_name. Will need to update script in the future
-  sample <- gsub('\\.fusions.annotated$', '', gsub('.*/', '', f))
-  segs <- read_tsv(f, col_names = TRUE, col_types = cols(.default = "c")) %>%
-    mutate(DepMap_ID=sample) %>% 
-    dplyr::select(DepMap_ID, everything())
-  
-  # Write to the output file
-  write.table(segs,  file=output_file_name,  sep='\t', row.names = FALSE, quote = FALSE, append = (i>1), col.names = !(i>1))
-}
-
-    """
     refwm = dm.WorkspaceManager(refworkspace)
     samples = refwm.get_samples()
     fusions_list = []
@@ -242,6 +204,7 @@ for(i in 1:nsamples){
 
     # removing failed
     fusions = fusions[~fusions[sampleCol].isin(todrop)]
+    fusions = fusions.drop(['JunctionReads','SpanningFrags'],axis =1)
 
     print("saving")
     fusions.to_csv(os.path.join(save_output, "fusions_all.csv"), index=False)
