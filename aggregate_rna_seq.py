56bd4f7725cbe5b019b3ebc8e849abb3653f7e6f
siyer-broad <169176197+siyer-broad@users.noreply.github.com>
2025-04-09 21:41:48 -0400

    Add files via upload
    Changes to aggregate fusions

--- aggregate_rna_seq.py
+++ aggregate_rna_seq.py
@@ -15,13 +15,13 @@ parser.add_argument("--terra_table", type=str, required=True, help='Terra table 
 parser.add_argument("--sample_metadata", type=str, required=True, help='Sample metadata file to use')
 
 args = parser.parse_args()
-terra_samples_file = args.terra_table
-sample_metadata_file = args.sample_metadata
-
+terra_samples_file = args.terra_table # test with terra_samples_file="/localstuff/terra_data_table_rnaseq_25q2.tsv"
+sample_metadata_file = args.sample_metadata # test with sample_metadata_file="/localstuff/2025-05-01-master-mapping-table_v4-internal-release-date-2025-05-01-master-mapping-table.csv"
+#export GOOGLE_APPLICATION_CREDENTIALS=/localstuff/depmap-omics-9764fbdbe040.json
 terra_samples = pd.read_table(terra_samples_file)
 samples_to_process_all = pd.read_csv(sample_metadata_file)
 
-samples_to_process = samples_to_process_all.loc[(samples_to_process_all["datatype"] == "rna") & (samples_to_process_all["is_default_entry"] == True)]
+samples_to_process = samples_to_process_all.loc[(samples_to_process_all["datatype"] == "rna")]
 samples = pd.merge(terra_samples, samples_to_process, left_on ="entity:sample_id", right_on="sequencing_id", how="inner")
 
 tc = create_taiga_client_v3()
@@ -57,6 +57,7 @@ model_cds_dict = dict(zip(samples["sequencing_id"],samples["model_id"]))
 cell_line_cds_dict = dict(zip(samples["sequencing_id"],samples["stripped_cell_line_name"]))
 code_cds_dict = dict(zip(samples["sequencing_id"],samples["depmap_code"]))
 lineage_cds_dict = dict(zip(samples["sequencing_id"],samples["lineage"]))
+is_default_cds_dict = dict(zip(samples["sequencing_id"],samples["is_default_entry"]))
 
 df_all_tpms = pd.DataFrame()
 df_all_counts = pd.DataFrame()
@@ -78,7 +79,7 @@ all_lengths_list = []
 sample_labels = []
 bigfusiontable = pd.DataFrame()
 
-for sample_id, sample_data in list(samples.iterrows()):
+for sample_id, sample_data in list(samples.iterrows())[:20]:
 	sample_key = sample_data["entity:sample_id"]
 	print(sample_id)
 	if pd.notna(sample_data["quant_genes"]):
@@ -98,25 +99,28 @@ for sample_id, sample_data in list(samples.iterrows()):
 		raise ValueError(sample_key + " does not have salmon output")
 	if pd.notna(sample_data["fusions"]):
 		fusiondf = pd.read_table(sample_data["fusions"])
+		# Use STAR read output to get total reads for normalization later
+		star_read_counts_df = pd.read_table(sample_data["reads_per_gene"], sep='\t',names=["colname","unstranded_counts","forward_stranded_counts","reverse_stranded_counts"])
+		total_reads = pd.Series.sum(star_read_counts_df["unstranded_counts"][4:])
 		if fusiondf.shape[0] > 0:
-			fusiondf["sample_id"] = sample_key
+			#fusiondf["sample_id"] = sample_key
 			fusiondf["profile_id"] = sample_data["profile_id"]
+			fusiondf["model_id"] = sample_data["model_id"]
+			fusiondf["cellline"] = sample_data["stripped_cell_line_name"]
+			fusiondf["oncotree_code"] = sample_data["depmap_code"]
+			fusiondf["lineage"] = sample_data["lineage"]
+			fusiondf["total_reads"] = total_reads
 			bigfusiontable = pd.concat([bigfusiontable, fusiondf], ignore_index=True)
-			# Take gene1 and gene2 columns and use hgnc term for it
 		else:
 			print("No fusions for " + sample_key)
 
 
-bigfusiontable["gene1"] = bigfusiontable["#gene1"].map(hgnc_names)
-bigfusiontable["gene2"] = bigfusiontable["gene2"].map(hgnc_names)
-bigfusiontable["gene1"] = bigfusiontable["gene1"].fillna(bigfusiontable["gene1"])
-bigfusiontable["gene2"] = bigfusiontable["gene2"].fillna(bigfusiontable["gene2"])
-# Create fused genename column by combining gene1 and gene2 separated by two hyphens
-bigfusiontable["fused_genename"] = bigfusiontable["gene1"] + "--" + bigfusiontable["gene2"]
+# Canonicalize gene fusion names, making it easier to group by
 
-# Calculate FFPM by summing (split_reads1+split_reads2)/(coverage1 + coverage2)
-bigfusiontable["FFPM"] = (bigfusiontable["split_reads1"] + bigfusiontable["split_reads2"]) / (bigfusiontable["coverage1"] + bigfusiontable["coverage2"])
-
+bigfusiontable['Canonical_FusionName'] = bigfusiontable.apply(lambda row: '--'.join(sorted([row['#gene1'], row['gene2']])), axis=1)
+# Calculate FFPM by summing (split_reads1+split_reads2)/total reads for sample and multiplying by 1e6
+bigfusiontable["fusion_reads"] = bigfusiontable["split_reads1"] + bigfusiontable["split_reads2"] + bigfusiontable["discordant_mates"]
+bigfusiontable["FFPM"] = 1e6*bigfusiontable["fusion_reads"]/bigfusiontable["total_reads"]
 
 # Convert lists to DataFrame at the end
 df_all_tpms = pd.DataFrame(np.column_stack(all_tpms_list), columns=sample_labels)
@@ -130,6 +134,7 @@ df_all_lengths.insert(0, 'Name', hgnc_names)
 df_all_tpms_cp = df_all_tpms.copy()
 df_all_counts_cp = df_all_counts.copy()
 df_all_lengths_cp = df_all_lengths.copy()
+
 upload_files = []
 df_dict = {
 	"df_tpms": df_all_tpms_cp,
@@ -155,7 +160,8 @@ for thisdfname, thisdf in df_dict.items():
 	model_id = thisdf.index.map(model_cds_dict)
 	depmap_code = thisdf.index.map(code_cds_dict)
 	lineage = thisdf.index.map(lineage_cds_dict)
-	metadatadf = pd.DataFrame({"seqid":thisdf.index, "profile_id":profile_id, "cellline":cellline, "model_id":model_id, "oncotree_code":depmap_code, "lineage":lineage})
+	is_default_entry = thisdf.index.map(is_default_cds_dict)
+	metadatadf = pd.DataFrame({"seqid":thisdf.index, "profile_id":profile_id, "is_default_entry":is_default_entry, "cellline":cellline, "model_id":model_id, "oncotree_code":depmap_code, "lineage":lineage})
 	metadatadf.set_index("seqid", inplace=True)
 	# Create an upload file for the full dataset, human and virus genes
 	thisdf.to_parquet(thisdfname + ".parquet", engine="pyarrow", index=False)
@@ -165,6 +171,20 @@ for thisdfname, thisdf in df_dict.items():
 		globals()[thisdfname + "_"+ df_output_name] = metadatadf.join(thisdf.iloc[:, df_output_indexes])
 		globals()[thisdfname + "_"+ df_output_name].to_parquet(thisdfname + "_"+ df_output_name+".parquet", engine="pyarrow", index=False)
 		upload_files.append(UploadedFile(name=thisdfname + "_"+ df_output_name, local_path=thisdfname + "_"+ df_output_name+".parquet", format=LocalFormat.PARQUET_TABLE))
-	
+
+bigfusiontable.rename(columns={"#gene1":"gene1", "total_reads":"Total Reads In Sample", "fusion_reads": "Total Reads Supporting Fusion"}, inplace=True)
+fusion_output_columns = ['profile_id', 'model_id', 'cellline', 'oncotree_code', 'lineage', 
+						 'Canonical_FusionName', 'gene1','gene2', 'Total Reads In Sample', 'Total Reads Supporting Fusion',  'FFPM', 
+						 'confidence','split_reads1', 'split_reads2', 'discordant_mates', 
+						 'strand1(gene/fusion)', 'strand2(gene/fusion)',  'reading_frame',
+						 'breakpoint1', 'breakpoint2', 'site1', 'site2', 'type', 'coverage1', 'coverage2',
+						 'tags', 'retained_protein_domains',
+						 'closest_genomic_breakpoint1', 'closest_genomic_breakpoint2',
+						 'gene_id1', 'gene_id2', 'transcript_id1', 'transcript_id2',
+						 'direction1', 'direction2', 'filters', 'fusion_transcript',
+						 'peptide_sequence']
+
+bigfusiontable[fusion_output_columns].to_parquet("SR_fusion_table.parquet", engine="pyarrow", index=False) 
+upload_files.append(UploadedFile(name="SR_fusion_table", local_path="SR_fusion_table.parquet", format=LocalFormat.PARQUET_TABLE))
 tc = create_taiga_client_v3()
 tc.create_dataset(name="25Q2 Expression Data Full Set",description="Testing upload of all expr files from new pipeline", files=upload_files)
